752a8e5874a27e02b4e88df3df4b38067fae2d60
J C <james@jamescampbell.us>
2016-11-29 15:11:40 -0500

    Update installer.py

--- installer.py
+++ installer.py
@@ -3,6 +3,6 @@
 # What: Runs the run.sh installer script
 import sys
 from subprocess import call
-call(["chmod", "a+x", "first-run/run.sh"])
-call(["./first-run/run.sh"])
+call(["chmod", "a+x", "run.sh"])
+call(["./run.sh"])
 exit("finis")
