985f7f70fbc905d9401c4147c211bca79b8f0a4f
J C <james@jamescampbell.us>
2016-11-29 11:19:39 -0500

    Update run.sh

--- first-run/run.sh
+++ first-run/run.sh
@@ -15,4 +15,5 @@ echo "installing zsh..."
 brew install zsh --upgrade
 echo "installing ohmyzsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp ../assets/zshrc ~/.zshrc
+mv $HOME/.zshrc $HOME/.zshrc-backup
+cp ../assets/zshrc $HOME/.zshrc
