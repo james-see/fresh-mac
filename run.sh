53512fb5b66705448605a21c83065d6b7728fbec
J C <james@jamescampbell.us>
2016-11-29 15:15:43 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -64,7 +64,7 @@ brew install zsh --upgrade
 echo "installing ohmyzsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 mv $HOME/.zshrc $HOME/.zshrc-backup
-cp assets/zshrc $HOME/.zshrc
+cp configs/zshrc $HOME/.zshrc
 echo "installing go..."
 brew install go --upgrade
 echo "installing python3..."
