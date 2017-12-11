97eb5a5ad1f239edc0520eebfa22c95edb5668d3
J C <james@jamescampbell.us>
2017-12-10 21:56:55 -0500

    Updated to accept light core option vs full option

--- run.sh
+++ run.sh
@@ -17,8 +17,9 @@ BACKTITLE="FRESH OSX - GET STARTED RIGHT BY JAMES CAMPBELL"
 TITLE="INSTALL YOUR BASIC SETUP NOW"
 MENU="Choose one of the following options:"
 
-OPTIONS=(1 "Install JC's default OSX setup"
-         2 "Quit now (too afraid)")
+OPTIONS=(1 "Install JC's OSX setup (batteries included)"
+         2 "Install the lite core version with just the basics"
+         3 "Quit now (too afraid)")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -32,35 +33,60 @@ clear
 case $CHOICE in
         1)
             echo "You chose Option 1"
+            $full = true;
             ;;
         2)
+            echo "You chose Option 2"
+            $lighter = true;
+        3)
             echo "You chose Option 2, goodbye."
             exit
             ;;
 esac
 brew tap caskroom/cask
 brew tap homebrew/services
-echo "installing php70 with fpm and mysql"
-brew tap homebrew/dupes && \
-  brew tap homebrew/php && \
-  brew install --without-apache --with-fpm --with-mysql php70
-echo "installing nginx..."
-brew tap homebrew/nginx && \
-brew install nginx
-echo "making sites-available dirs..."
-  mkdir -p /usr/local/etc/nginx/sites-available && \
-  mkdir -p /usr/local/etc/nginx/sites-enabled && \
-  mkdir -p /usr/local/etc/nginx/conf.d && \
-  mkdir -p /usr/local/etc/nginx/ssl
-echo "installing task warrior"
-brew install task
-brew install taskd
-brew install tasksh
-echo "installing git..."
-brew install git
-echo "installing sublime text..."
-brew install Caskroom/cask/sublime-text
-brew install bash-completion
+if [ "$full" = true ] ; then
+    echo "installing php70 with fpm and mysql"
+    brew tap homebrew/dupes && \
+    brew tap homebrew/php && \
+    brew install --without-apache --with-fpm --with-mysql php70
+    echo "installing nginx..."
+    brew tap homebrew/nginx && \
+    brew install nginx
+    echo "making sites-available dirs..."
+    mkdir -p /usr/local/etc/nginx/sites-available && \
+    mkdir -p /usr/local/etc/nginx/sites-enabled && \
+    mkdir -p /usr/local/etc/nginx/conf.d && \
+    mkdir -p /usr/local/etc/nginx/ssl
+    echo "installing task warrior"
+    brew install task
+    brew install taskd
+    brew install tasksh
+    echo "installing git..."
+    brew install git
+    echo "installing sublime text..."
+    brew install Caskroom/cask/sublime-text
+    brew install bash-completion
+    echo "installing nodejs..."
+    brew install node
+    brew install npm
+    echo "installing hgrep from npm..."
+    npm install -g hgrep
+    echo "installing charles..."
+    brew install Caskroom/cask/charles
+    echo "installing little snitch..."
+    brew install Caskroom/cask/little-snitch
+    echo "installing htop..."
+    brew install htop
+    echo "installing tmux..."
+    brew install tmux
+    echo "installing imagemagick..."
+    brew install imagemagick
+    echo "installing Panic's Coda..."
+    brew install Caskroom/cask/coda
+    echo "installing bartender..."
+    brew install Caskroom/cask/bartender
+fi
 echo "installing iterm2..."
 brew install Caskroom/cask/iterm2
 echo "installing gpgtools..."
@@ -79,29 +105,10 @@ echo "installing fav fonts..."
 cp fonts/*.ttf /Library/Fonts/
 echo "installing Chromium..."
 brew install Caskroom/cask/chromium
-echo "installing unetbootin..."
-brew install Caskroom/cask/unetbootin
-echo "installing nodejs..."
-brew install node
-brew install npm
-echo "installing hgrep from npm..."
-npm install -g hgrep
-echo "installing charles..."
-brew install Caskroom/cask/charles
-echo "installing little snitch..."
-brew install Caskroom/cask/little-snitch
-echo "installing htop..."
-brew install htop
-echo "installing tmux..."
-brew install tmux
-echo "installing imagemagick..."
-brew install imagemagick
+echo "installing etcher..."
+brew install Caskroom/cask/etcher
 echo "installing tunnelblick..."
 brew install Caskroom/cask/tunnelblick
-echo "installing Panic's Coda..."
-brew install Caskroom/cask/coda
-echo "installing bartender..."
-brew install Caskroom/cask/bartender
 echo "installing all pip packages..."
 cat requirements.txt | sudo xargs -n 1 pip3 install
 echo "changing default screenshot location to ~/Documents/Screenshots"
