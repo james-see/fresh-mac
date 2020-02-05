d2b29ed39bdd834c0d1a6baa49ef9f318eb9d78c
J C <james@jamescampbell.us>
2020-02-05 16:23:23 -0500

    Update installer.py

--- installer.py
+++ installer.py
@@ -2,7 +2,9 @@
 # Author: James Campbell
 # What: Runs the run.sh installer script and copies the zsh file into place
 import sys
+import os
 from subprocess import call
+os.environ['PHONE'] = str(sys.argv[1])
 call(["./run.sh"])
 call(["mv", "$HOME/.zshrc", "$HOME/.zshrc-backup"])
 call(["cp", "./configs/zshrc", "$HOME/.zshrc"])
